cfa474f1dc65a570fea2a5eb00f8d7ab69498ee8
William Chrapcynski <wchrapcynski@gmail.com>
2020-04-25 20:50:02 -0400

    Added category to model

--- game/models.py
+++ game/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django_mysql.models import ListTextField
 
 class TriviaQuestion(models.Model):
-    question = models.TextField()
+    question = models.TextField(null=True)
     answers = ListTextField(
         base_field=models.CharField(max_length=300),
         size=4,
         max_length=(6 * 11)
     )
+    category = models.CharField(max_length=300, null=True)
     published = models.BooleanField(default=False)
 
     def __str__(self):
